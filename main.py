661f652da35995cb96412b10ef994cae559efa86
Vul <103326409+vulpium@users.noreply.github.com>
2024-04-02 05:11:13 +0300

    More cleanup + Bug fix
    This commit resolves a bug that would cause proxy validity checks to go on forever

--- main.py
+++ main.py
@@ -1,66 +1,16 @@
-from imports.proxyScraper import ProxyScraper
-from imports.locationChecker import LocationChecker
-from imports.proxyChecker import ProxyChecker
-
-import threading
-
 import random
 import flask
-import time
 
-class ProxyService:
-    def __init__(self):
-        self.forceRefresh = False
-        self.validProxys = []
-
-    def proxyRefresh(self) -> None:
-        locateInstance = LocationChecker(ValidLocations = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE"])
-        scrapeInstance = ProxyScraper()
-        checkerInstance = ProxyChecker()
-
-        lastUpdate = 0
-
-        while True:
-            if time.time() - lastUpdate > 300 or self.forceRefresh:
-
-                self.forceRefresh = False
-                lastUpdate = time.time()
-
-                proxys = scrapeInstance.scrape(saveAsFile=False)
-
-                locateInstance.setList(proxys)
-                locateInstance.massIsLocation()
-                locatedProxies = locateInstance.getValid()
-
-                checkerInstance.setList(locatedProxies)
-                checkerInstance.massCheck()
-
-                self.validProxys = checkerInstance.getValid()
-                print(f"Proxy list updated! amount: {len(self.validProxys)}")
-
-
-    def getValid(self) -> list:
-        return self.validProxys
-
-    def triggerForceRefresh(self) -> bool:
-        self.forceRefresh = True
-        return True
-    
-    def getArrayLenght(self) -> int:
-        return len(self.validProxys)
-
-    def startServer(self) -> None:
-        server.run(debug=True, host='0.0.0.0')
+from imports.proxyService import ProxyService
 
 
 server = flask.Flask(__name__)
-proxyServiceInstance = ProxyService()
+proxyServiceInstance = ProxyService(server)
 
 
 @server.route("/")
 def index():
     return ""
-
 
 @server.route("/proxy", methods = ['GET'])
 def proxy():
@@ -75,11 +25,9 @@ def proxy():
         "proxy": random.choice(proxys)
     }
 
-
 @server.route("/proxy_array_length", methods = ['GET'])
 def proxy_array_lenght():
     return str(proxyServiceInstance.getArrayLenght())
-
 
 @server.route("/proxy_array_force_refresh", methods = ['POST'])
 def proxy_array_force_refresh():
@@ -88,10 +36,7 @@ def proxy_array_force_refresh():
 
 if __name__ == "__main__":
 
-    cool = threading.Thread(target=proxyServiceInstance.proxyRefresh)
-    cool.start()
-
+    with server.app_context():
+        proxyServiceInstance.proxyRefresh()
+    
     proxyServiceInstance.startServer()
-
-    while True:
-        time.sleep(1)
